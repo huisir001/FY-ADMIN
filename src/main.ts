a53077d3f1e4fdf8908a332d7a6898ea3804bca1
huisir <273250950@qq.com>
2021-09-01 18:44:17 +0800

    -

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
+/*
+ * @Description: 入口
+ * @Autor: HuiSir<273250950@qq.com>
+ * @Date: 2021-09-01 16:37:47
+ * @LastEditTime: 2021-09-01 18:26:35
+ */
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import elemUi from './ui'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App)
+    .use(store)
+    .use(router)
+    .use(elemUi)
+    .mount('#app')
---
+++ src/ui/index.ts
@@ -0,0 +1,64 @@
+/*
+ * @Description:ElementUI按需引入
+ * @Autor: HuiSir<273250950@qq.com>
+ * @Date: 2021-09-01 17:33:54
+ * @LastEditTime: 2021-09-01 18:38:35
+ */
+
+/* 使用vue插件方式导出组件，插件API：https://v3.cn.vuejs.org/guide/plugins.html */
+
+import { App, Plugin } from '@vue/runtime-core'
+
+import {
+    ElButton,
+    ElForm,
+    ElFormItem,
+    ElInput,
+    ElInputNumber,
+    ElTable,
+    ElTableColumn,
+    ElLoading,
+    ElDivider,
+    ElPopover,
+    ElSwitch,
+    ElDatePicker,
+    ElPagination,
+    ElDescriptions,
+    ElDescriptionsItem
+} from 'element-plus'
+
+// 样式
+import './variables.scss'
+
+const components = [
+    ElButton,
+    ElForm,
+    ElFormItem,
+    ElInput,
+    ElInputNumber,
+    ElTable,
+    ElTableColumn,
+    ElLoading,
+    ElDivider,
+    ElPopover,
+    ElSwitch,
+    ElDatePicker,
+    ElPagination,
+    ElDescriptions,
+    ElDescriptionsItem
+]
+
+interface iElementUIOption {
+    size: string
+    zIndex: number
+}
+
+export default (app: App<any>, option: iElementUIOption = { size: 'small', zIndex: 3000 }) => {
+    // 配置项
+    app.config.globalProperties.$ELEMENT = option
+
+    // 组件注册
+    components.forEach((component) => app.use(component as Plugin))
+
+    return app
+}
