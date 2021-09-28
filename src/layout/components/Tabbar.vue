11d1fed0a7b48910408c9d1717cb781bb771e32a
huisir <273250950@qq.com>
2021-09-28 19:44:46 +0800

    -

--- src/layout/components/Tabbar.vue
+++ src/layout/components/Tabbar.vue
@@ -2,13 +2,12 @@
  * @Description: Tabbar
  * @Autor: HuiSir<273250950@qq.com>
  * @Date: 2021-09-10 18:50:20
- * @LastEditTime: 2021-09-28 18:59:46
+ * @LastEditTime: 2021-09-28 19:39:32
 -->
 <template>
     <div class="tabbar">
-        <el-tabs v-model="editableTabsValue" type="card" closable>
-            <el-tab-pane v-for="item in dynamicTags" :key="item.name" :label="item.name"
-                :name="item.name">
+        <el-tabs type="card" closable>
+            <el-tab-pane v-for="item in dynamicTags" :key="item.name" :label="item.name" lazy>
             </el-tab-pane>
         </el-tabs>
     </div>
@@ -37,6 +36,46 @@ export default defineComponent({
                 name: 444,
                 active: false,
             },
+            {
+                name: 555,
+                active: false,
+            },
+            {
+                name: 666,
+                active: false,
+            },
+            {
+                name: 777,
+                active: false,
+            },
+            {
+                name: 888,
+                active: false,
+            },
+            {
+                name: 999,
+                active: false,
+            },
+            {
+                name: 100,
+                active: false,
+            },
+            {
+                name: 101,
+                active: false,
+            },
+            {
+                name: 102,
+                active: false,
+            },
+            {
+                name: 103,
+                active: false,
+            },
+            {
+                name: 104,
+                active: false,
+            },
         ])
 
         const tagChange = (index: number) => {
@@ -53,28 +92,50 @@ export default defineComponent({
 </script>
  
 <style scoped lang="scss">
+$--tab-height: 30px;
 .tabbar {
-    height: 30px;
+    height: $--tab-height;
     width: 100%;
     display: flex;
     align-items: center;
     border-radius: var(--el-border-radius-small);
     &:deep(.el-tabs) {
+        width: 100%;
         .el-tabs__header {
             border: 0;
             margin: 0;
         }
+        .el-tabs__nav-next,
+        .el-tabs__nav-prev {
+            line-height: $--tab-height;
+            height: $--tab-height;
+            background-color: var(--color-tabbar-item-bg);
+            border-radius: var(--el-border-radius-small);
+            width: 20px;
+            text-align: center;
+            font-size: 14px;
+        }
+        .el-tabs__nav-wrap.is-scrollable {
+            padding: 0 30px;
+        }
         .el-tabs__item {
-            background-color: #fff;
+            background-color: var(--color-tabbar-item-bg);
             margin-right: 10px;
             border: 0;
             border-radius: var(--el-border-radius-small);
-            height: 30px;
-            line-height: 30px;
+            height: $--tab-height;
+            line-height: $--tab-height;
             padding: 0 10px;
             .el-icon-close {
                 margin-left: 3px;
                 line-height: 14px;
+                border-radius: 0;
+                &:hover {
+                    background-color: var(--el-color-primary);
+                }
+            }
+            &:last-child {
+                margin-right: 0;
             }
         }
         .el-tabs__nav {
