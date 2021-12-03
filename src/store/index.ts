3d3787a4f9013501757a7ff698375de393f1692d
huisir <273250950@qq.com>
2021-12-03 17:54:26 +0800

    chore: 注释完善

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,7 +2,7 @@
  * @Description: store
  * @Autor: HuiSir<273250950@qq.com>
  * @Date: 2021-09-01 16:37:47
- * @LastEditTime: 2021-10-21 22:22:18
+ * @LastEditTime: 2021-12-03 17:54:12
  */
 import { InjectionKey } from 'vue'
 import { createStore, useStore as baseUseStore, Store } from 'vuex'
@@ -23,6 +23,7 @@ export const storeKey: InjectionKey<Store<IState>> = Symbol()
 
 // 定义自己的 `useStore` 组合式函数
 // 将类型化的 InjectionKey 传给 useStore 方法
+// 这样搞是为了让vuex支持ts类型，便与开发
 export const useStore = () => baseUseStore(storeKey)
 
 // 导出Store
