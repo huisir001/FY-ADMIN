47167ae71888386fbee240923028ba0ef770b3a6
huisir <273250950@qq.com>
2022-01-04 16:19:39 +0800

    refactor: 重构全局自定义组件的注册

--- src/ui/fy/index.ts
+++ src/ui/fy/index.ts
@@ -2,7 +2,7 @@
  * @Description: 个人组件封装
  * @Autor: HuiSir<273250950@qq.com>
  * @Date: 2021-09-01 17:33:54
- * @LastEditTime: 2021-12-30 14:11:02
+ * @LastEditTime: 2022-01-04 16:15:06
  */
 import { App } from 'vue'
 import Icon from './Icon/index.vue'
@@ -15,7 +15,7 @@ import IconSelect from './IconSelect/index.vue'
 import TreeSelect from './TreeSelect/index.vue'
 
 
-const components = [
+const components: IObj = {
     Icon,
     Table,
     RowBtns,
@@ -24,14 +24,15 @@ const components = [
     FileLibrary,
     IconSelect,
     TreeSelect
-]
+}
 
 export default (app: App<any>) => {
 
     // 组件注册
-    components.forEach((component) => {
-        app.component(`Fy${component.name}`, component)
-    })
+    for (const key in components) {
+        const component = components[key];
+        app.component(`Fy${key}`, component)
+    }
 
     return app
 }
