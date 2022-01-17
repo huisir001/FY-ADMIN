a01e0840c04a7ca91892785a114480d0884b3c9b
huisir <273250950@qq.com>
2022-01-17 16:37:09 +0800

    feat: 首页完善

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,21 +2,21 @@
  * @Description: 首页
  * @Autor: HuiSir<273250950@qq.com>
  * @Date: 2021-09-09 15:14:07
- * @LastEditTime: 2022-01-17 16:23:05
+ * @LastEditTime: 2022-01-17 16:28:29
 -->
 <template>
     <el-row :gutter="10">
         <el-col :span="6">
-            <div class="grid-content bg-purple"></div>
+            <div class="grid-content"></div>
         </el-col>
         <el-col :span="6">
-            <div class="grid-content bg-purple"></div>
+            <div class="grid-content"></div>
         </el-col>
         <el-col :span="6">
-            <div class="grid-content bg-purple"></div>
+            <div class="grid-content"></div>
         </el-col>
         <el-col :span="6">
-            <div class="grid-content bg-purple"></div>
+            <div class="grid-content"></div>
         </el-col>
     </el-row>
 </template>
@@ -25,10 +25,8 @@
 </script>
  
 <style scoped lang="scss">
-.bg-purple {
-    background: #fff;
-}
 .grid-content {
+    background: #fff;
     border-radius: 4px;
     min-height: 200px;
 }
