5d08bc1788ad731fa3b5c9c5bf8297aacbde9b6f
huisir <273250950@qq.com>
2022-02-07 16:44:17 +0800

    refactor: 将vue相关依赖项用CDN导入取代包管理(生产环境)

--- settings/cdn.ts
+++ settings/cdn.ts
@@ -2,18 +2,48 @@
  * @Description: CDN Resources
  * @Autor: HuiSir<273250950@qq.com>
  * @Date: 2022-01-24 15:59:14
- * @LastEditTime: 2022-01-24 16:00:07
+ * @LastEditTime: 2022-02-07 16:41:21
  */
+
+type tCssCdns = string[]
+interface iJsCdns {
+    dependencies: string // 外部化处理不想打包进库的依赖
+    src: string // 外部cdn链接
+    globalVar: string  // cdn包取代依赖项的全局变量
+}
 
 /**
  * CSS CDN Resources
  */
-export const CSSCDN = [
+export const CSSCDN: tCssCdns = [
     '//unpkg.com/element-plus/dist/index.css',   // element-plus 
 ]
 
 /**
  * JS CDN Resources
- * 后期考虑将vue/vue-router/element-plus/vuex等插件直接使用cdn引入
+ * 将vue/vue-router/element-plus/vuex等插件直接使用cdn引入
+ * 需要配置vite.config.js，将不需要打包的库排除，且给CDN导入的外部包一个全局变量
+ * 注意：vue-router/element-plus/vuex等库须依赖vue库，所以vue需要在前面
  */
-export const JSCDN = []
+export const JSCDN: iJsCdns[] = [
+    {
+        dependencies: 'vue',
+        src: '//unpkg.com/vue@next',
+        globalVar: 'Vue'
+    },
+    {
+        dependencies: 'element-plus',
+        src: '//unpkg.com/element-plus',
+        globalVar: 'ElementPlus'
+    },
+    {
+        dependencies: 'vue-router',
+        src: '//unpkg.com/vue-router',
+        globalVar: 'VueRouter'
+    },
+    {
+        dependencies: 'vuex',
+        src: '//unpkg.com/vuex',
+        globalVar: 'Vuex'
+    }
+]
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@
  * @Description: vite config
  * @Autor: HuiSir<273250950@qq.com>
  * @Date: 2022-01-24 14:04:37
- * @LastEditTime: 2022-01-25 11:05:16
+ * @LastEditTime: 2022-02-07 16:31:57
  */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -31,7 +31,8 @@ export default defineConfig(({ mode }) => {
         injectData: {
           TITLE,
           CSSCDN,
-          JSCDN
+          // 打包时再导入外部CDN，开发环境使用包管理
+          JSCDN: loadEnv(mode, process.cwd()).VITE_NODE_ENV == 'production' ? JSCDN : []
         }
       })
     ],
@@ -42,6 +43,21 @@ export default defineConfig(({ mode }) => {
     },
     json: {
       stringify: true // 导入的 JSON 会被转换为 export default JSON.parse('...')
+    },
+    // 打包配置
+    build: {
+      // 压缩
+      minify: "esbuild",
+      sourcemap: false, // 输出.map文件
+      rollupOptions: {
+        // 确保外部化处理不想打包进库的依赖
+        // 外部包
+        external: JSCDN.map(item => item.dependencies),
+        output: {
+          // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+          globals: getOutputGlobals(JSCDN)
+        }
+      }
     },
     server: {
       host: '127.0.0.1',
@@ -74,3 +90,11 @@ export default defineConfig(({ mode }) => {
     }
   }
 })
+
+function getOutputGlobals (cdnObjs) {
+  const outputGlobals = {}
+  cdnObjs.forEach(item => {
+    outputGlobals[item.dependencies] = item.globalVar
+  })
+  return outputGlobals
+}
