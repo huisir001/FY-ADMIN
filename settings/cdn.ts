688478dc852ab62c24e972f794f7d11f39e5bbf9
huisir <273250950@qq.com>
2022-02-08 11:32:55 +0800

    feat: 配置项注释补充

--- settings/cdn.ts
+++ settings/cdn.ts
@@ -2,7 +2,7 @@
  * @Description: CDN Resources
  * @Autor: HuiSir<273250950@qq.com>
  * @Date: 2022-01-24 15:59:14
- * @LastEditTime: 2022-02-07 16:41:21
+ * @LastEditTime: 2022-02-08 11:32:19
  */
 
 type tCssCdns = string[]
@@ -23,7 +23,8 @@ export const CSSCDN: tCssCdns = [
  * JS CDN Resources
  * 将vue/vue-router/element-plus/vuex等插件直接使用cdn引入
  * 需要配置vite.config.js，将不需要打包的库排除，且给CDN导入的外部包一个全局变量
- * 注意：vue-router/element-plus/vuex等库须依赖vue库，所以vue需要在前面
+ * 注意1：vue-router/element-plus/vuex等库须依赖vue库，所以vue需要在前面
+ * 注意2：外链cdn注意版本问题，需与package.json中的包版本一致
  */
 export const JSCDN: iJsCdns[] = [
     {
