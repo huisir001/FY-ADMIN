a560e051a5ffdc458195afee15968d6c6af64b69
huisir <273250950@qq.com>
2022-02-16 18:19:14 +0800

    feat: 富文本編輯器開發

--- src/views/examples/basicComp/editor.vue
+++ src/views/examples/basicComp/editor.vue
@@ -2,12 +2,15 @@
  * @Description: 编辑器
  * @Autor: HuiSir<273250950@qq.com>
  * @Date: 2022-02-16 14:29:02
- * @LastEditTime: 2022-02-16 18:13:06
+ * @LastEditTime: 2022-02-16 18:18:04
 -->
 <template>
     <div class="my-editor">
         <fy-editor />
     </div>
+    <fy-card :type="2" title="HTML内容">
+        {{htmlVal}}
+    </fy-card>
 </template>
 
 <script lang="ts">
@@ -17,11 +20,17 @@ export default {
 </script>
 
 <script lang="ts" setup>
+import { ref } from 'vue'
+const htmlVal = ref('')
 </script>
 
 <style scoped lang="scss">
 .my-editor {
+    padding: 20px;
     width: 100%;
     height: 500px;
+    background: #fff;
+    border-radius: 4px;
+    margin-bottom: 10px;
 }
 </style>
